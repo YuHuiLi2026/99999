bc061ea48651988ea7e36d0e93daa4029d5311ef
YuHuiLi2026 <111806126+YuHuiLi2026@users.noreply.github.com>
2022-10-13 12:39:00 -0700

    Update part2.cc

--- part2.cc
+++ part2.cc
@@ -21,17 +21,17 @@ int main(int argc, char const *argv[]) {
   std::cout << "Hi - I'm a computer and I've thought of a number between "
                "1 and 100.\n";
   std::cout << "Let's play a guessing game...\n";
-  std::cout << "What's your guess?> ";
-  std::cin >> guess;
   while (!game_over) {
+    std::cout << "What's your guess?> ";
+    std::cin >> guess;
     if (guess == kSecretNumber) {
       std::cout << "\tHooray! You guessed the secret number!!";
       std::cout << "Do you want to play again? (y or n)>";
       std::cin >> answer;
-      if (answer == "y") {
-        std::cout << "Great!! I'd love to play again...";
-      } else {
+      if (answer != "y") {
         game_over = true;
+      } else {
+        std::cout << "Great!! I'd love to play again...";
       }
     }
     if (guess < 1 || guess > 100) {
@@ -40,9 +40,11 @@ int main(int argc, char const *argv[]) {
       int current_guess = guess;
       if (std::abs(kSecretNumber - current_guess) <
           std::abs(kSecretNumber - last_guess)) {
-        std::cout << "You're getting warmer!\n";
+        std::cout << "\tNope - that's not it.\n";
+        std::cout << "\tYou're getting warmer!\n";
       } else {
-        std::cout << "You're getting colder.\n";
+        std::cout << "\tNope - that's not it.\n";
+        std::cout << "\tYou're getting colder.\n";
       }
       last_guess = current_guess;
     }
